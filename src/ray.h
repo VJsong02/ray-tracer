3aeb74683a87419bbd7c9f47ad044a6a98d47c90
VJsong02 <victor.song.02@gmail.com>
2019-04-30 18:15:10 +0200

    compileable program

--- src/ray.h
+++ src/ray.h
@@ -1,62 +1,60 @@
 #pragma once
 
-#include <iostream>
+#include <ostream>
 
 namespace Ray
 {
-template <typename T>
-class vector
-{
-  protected:
-    T dim[3]; // Vector dimensions
+	template <typename T>
+	class vector
+	{
+	protected:
+		T dim[3]; // Vector dimensions
 
-  public:
-    vector(T x, T y, T z)
-    {
-        dim[0] = x;
-        dim[1] = y;
-        dim[2] = z;
-    }
+	public:
+		vector(T x, T y, T z)
+		{
+			dim[0] = x;
+			dim[1] = y;
+			dim[2] = z;
+		}
 
-    T get_vector(unsigned char d) { return const dim[d]; }
-};
+		T get_vector(unsigned char d) { return dim[d]; }
 
-template <typename T>
-class ray : public vector<T>
-{
-  protected:
-    unsigned char brightness; // ray brightness
+		friend std::ostream& operator<<(std::ostream& os, Ray::vector<T>& v)
+		{
+			return os << "(" << v.get_vector(0) << " " << v.get_vector(1) << " " << v.get_vector(2) << ")";
+		}
+	};
 
-  public:
-    ray(T x, T y, T z, unsigned char b)
-        : vector<T>(x, y, z)
-    {
-        brightness = b;
-    }
+	template <typename T>
+	class ray : public vector<T>
+	{
+	protected:
+		unsigned char brightness; // ray brightness
 
-    unsigned char get_brightness() { return const brightness; }
-};
+	public:
+		ray(T x, T y, T z, unsigned char b)
+			: vector<T>(x, y, z)
+		{
+			brightness = b;
+		}
 
-template <typename T>
-bool reflect(ray<T> &a, ray<T> &b)
-{
-}
+		unsigned char get_brightness() { return brightness; }
 
-template <typename T>
-bool refract(ray<T> &a, ray<T> &b)
-{
-}
+		friend std::ostream& operator<<(std::ostream& os, Ray::ray<T>& r)
+		{
+			return os << "(" << r.get_vector(0) << " " << r.get_vector(1) << " " << r.get_vector(2) << ") " << +r.get_brightness() << "%";
+		}
+	};
+
+	template <typename T>
+	bool reflect(ray<T>& a, ray<T>& b)
+	{
+	}
+
+	template <typename T>
+	bool refract(ray<T>& a, ray<T>& b)
+	{
+	}
 
 } // namespace Ray
-
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const Ray::vector<T> &v)
-{
-    return os << "(" << v.get_vector(0) << " " << v.get_vector(1) << " " << v.get_vector(2) << ")";
-}
-
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const Ray::ray<T> &r)
-{
-    return os << "(" << r.get_vector(0) << " " << r.get_vector(1) << " " << r.get_vector(2) << ") " << r.get_brightness() << "%";
-}
