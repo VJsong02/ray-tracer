6c66e2f2a98458195a0444c6b2a2a0b778e26db0
VJsong02 <victor.song.02@gmail.com>
2019-04-30 09:38:29 +0200

    Fix majority of errors

--- src/ray.h
+++ src/ray.h
@@ -4,26 +4,64 @@
 
 namespace Ray
 {
-    template <typename T>
-    class ray
-    {
-        T vector[3];              // vector size
-        unsigned char brightness; // vector brightness
+	template <typename T>
+	class vector
+	{
+	protected:
+		T vector[3]; // Vector dimensions
 
-    public:
-        ray(T x, T y, T z, unsigned char brightness)
-        {
-            vector[0] = x;
-            vector[1] = y;
-            vector[2] = z;
-            brightness = b;
-        }
+	public:
+		vector(T x, T y, T z)
+		{
+			vector[0] = x;
+			vector[1] = y;
+			vector[2] = z;
+		}
 
-        friend ostream &operator<<(ostream &os, const ray &r)
-        {
-            return os << "(" << r.vector[0] << " " << r.vector[1] << " " << r.vector[2] << ") " << +brightness << "%" << endl;
-        }
-    };
+		T get_vector(unsigned char d) { return vector[d]; }
+	};
 
-    
+	template <typename T>
+	class ray : public vector<T>
+	{
+	protected:
+		unsigned char brightness; // ray brightness
+
+	public:
+		ray(T x, T y, T z, unsigned char b)
+		{
+			vector[0] = x;
+			vector[1] = y;
+			vector[2] = z;
+			brightness = b;
+		}
+
+		unsigned char get_brightness() { return brightness; }
+	};
+
+	template <typename T>
+	bool reflect(ray<T>& a, ray<T>& b)
+	{
+
+	}
+
+	template <typename T>
+	bool refract(ray<T>& a, ray<T>& b)
+	{
+
+	}
+
+
 } // namespace Ray
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Ray::vector<T>& v)
+{
+	return os << "(" << v.get_vector(0) << " " << v.get_vector(1) << " " << v.get_vector(2) << ")";
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Ray::ray<T>& r)
+{
+	return os << "(" << r.get_vector(0) << " " << r.get_vector(1) << " " << r.get_vector(2) << ") " << r.get_brightness() << "%";
+}
